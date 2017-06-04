520731370c70340e4b9a718c1c7a237703bbb738
David Merrick <david.merrick@jivesoftware.com>
2017-06-04 09:19:39 -0700

    Added error handling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import ClassifierUtils from '../src/utils/ClassifierUtils'
 import TwitterUtils from '../src/utils/TwitterUtils'
 
 exports.handler = async function index(event, context, callback){
+    let {category, sentimentLabel} = event;
+    if(!category || !sentimentLabel){
+        return callback(new Error("Please specify category and sentimentLabel"));
+    }
+
     let screenName = event.screen_name || process.env.SCREEN_NAME;
     let tweets = await TwitterUtils.getTweets(screenName);
     let filteredTweets = TwitterUtils.filterOutRetweets(tweets);
@@ -9,9 +14,9 @@ exports.handler = async function index(event, context, callback){
     for(var i = 0; i < filteredTweets.length; i++){
         let tweet = filteredTweets[i];
         let classification = await ClassifierUtils.classifyText(tweet.text);
-        let foundCategory = classification.categories.find(category => category.label.indexOf(event.category) != -1);
-        let sentimentLabel = classification.sentiment.document.label;
-        let matches = foundCategory != null && sentimentLabel === event.sentimentLabel;
+        let foundCategory = classification.categories.find(item => item.label.indexOf(category) != -1);
+        let document = classification.sentiment.document;
+        let matches = foundCategory != null && document.label === sentimentLabel;
         if(matches){
             foundTweet = filteredTweets[i];
             break;
